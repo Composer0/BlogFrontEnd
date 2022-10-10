9562b12279a6b69a087094f21e424d9672206375
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-10-09 22:36:14 -0400

    Login jsx added
    working on login css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import NavBar from './components/NavBar/NavBar'
 // import Home from './pages/home/Home'
 // import Single from './pages/single/Single'
 // import Write from './pages/write/Write'
-import Settings from './pages/settings/Settings'
+// import Settings from './pages/settings/Settings'
+import Login from './pages/login/Login'
 
 function App() {
     return (
@@ -13,7 +14,8 @@ function App() {
             {/* <Single /> */}
             {/* <Home /> */}
             {/* <Write /> */}
-            <Settings />
+            {/* <Settings /> */}
+            <Login />
 
         </>
         </div>
