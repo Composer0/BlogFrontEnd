e588721e1a64b0ebbc15fcf33297c30bcaaca2c2
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-10-11 14:06:02 -0400

    Day 6 update
    Added new folders for Contact and About Page
    Added react-router-dom throughout the project. Links can now successfully navigate between the pages while also checking to see if the user is logged in or not.
    Began incorporating a href links into the project for the social media icons.
    Created NavBar variation for user not being logged in.
    Learned about how Routes replaced switch and in fact makes it easier to write out the route. Used element inside of route to indicate the proper route and thus saving one line of code for each route when implemented.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ import Write from './pages/write/Write'
 import Settings from './pages/settings/Settings'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom"
 
 function App() {
+    const user = true;
     return (
         <div className='App'>
-        <>
-
+        <Router>
             <NavBar />
-            <Home />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/post/:postId" element={<Single />} />
+                <Route path="/write" element={user ? <Write /> : <Register />} />
+                <Route path="/settings" element={user ? <Settings /> : <Register />} />
+                <Route path="/login" element={user ? <Home /> : <Login />} />
+                <Route path="/register" element={user ? <Home/> : <Register />} />
+            </Routes>
+
             {/* <Single /> */}
             {/* <Write /> */}
             {/* <Settings /> */}
             {/* <Login /> */}
             {/* <Register /> */}
 
-        </>
+        </Router>
         </div>
     )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,58 @@
+import {Link} from "react-router-dom";
 import "./navbar.css"
 
 export default function NavBar() {
+    const user = true;
   return (
     <div className="nav">
         <div className="navLeft">
-            <i className="navIcon fab fa-facebook-square"></i>
+                <a href="https://www.facebook.com/D27.Eternal/" target="_blank">
+            <i className="navIcon fab fa-facebook-square">
+
+            </i>
+                </a>
             <i className="navIcon fab fa-github-square"></i>
             <i className="navIcon fab fa-linkedin"></i>
             <i className="navIcon fa-brands fa-hashnode"></i>
         </div>
         <div className="navCenter">
             <ul className="navList">
-                <li className="navListItem">HOME</li>
-                <li className="navListItem">ABOUT</li>
-                <li className="navListItem">CONTACT</li>
-                <li className="navListItem">WRITE</li>
-                <li className="navListItem">LOGOUT</li>
+                <li className="navListItem">
+                    <Link className="link" to="/">HOME</Link>
+                </li>
+                <li className="navListItem">
+                    <Link className="link" to="/">ABOUT</Link>
+                </li>
+                <li className="navListItem">
+                    <Link className="link" to="/">CONTACT</Link>
+                </li>
+                <li className="navListItem">
+                    <Link className="link" to="/write">WRITE</Link>
+                </li>
+                <li className="navListItem">
+                {user && "LOGOUT"}
+                </li>
             </ul>
         </div>
         <div className="navRight">
-            <img className="navImg" src="https://www.orionpalmer.com/static/media/orionpalmer.ba51d754.webp" alt="user"></img>
+            {
+                user ? (
+
+                    <Link to="/settings">
+                    <img 
+                        className="navImg" src="https://www.orionpalmer.com/static/media/orionpalmer.ba51d754.webp" alt="user">
+                    </img>
+                    </Link>
+                ) : (
+                    <ul className="navListItem">
+
+                        <Link className="link" to="/login">LOGIN</Link>
+                        <Link className="link" to="/register">REGISTER</Link>
+                    </ul>
+                )
+
+            }
+
             <i className="navSearchIcon fas fa-search"></i>
         </div>
     </div>
