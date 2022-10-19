05a8cac051bb903803b9dc4a025c8d2ebe91583c
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-10-19 19:44:35 -0400

    Update About page. Needs a hover card to display the rest of the text that is cut off.

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,40 +2,42 @@ import "./about.css"
 
 export default function About() {
     return (
-    <div className="about">
-      <h1>So you want to know more...</h1>
-      <section className="section-tours" id="section-tours">
-                <div id="tours" className="u-center-text u-margin-bottom-big">
-                    <h2 className="heading-secondary">
-                        about Orion Palmer.
-                    </h2>
-                </div>
-                <div className="row">
-                    <div className="col-1-of-3">
-                        <div className="card">
-                            <div className="card__side card__side--front card__side--front-1">
-                                <div className="card__picture card__picture--1">
-
+        <>
+        <div className="about">
+            <h1 className="aboutHeader">ABOUT</h1>
+            <h2 className="aboutHeaderSecondary">
+                Orion Palmer
+            </h2>
+        </div>
+                    <div className="overseer">
+                        <div className="container">
+                            <div className="card musician">
+                                <h1 className="title">Musician</h1>
+                                <div className="information">
+                                    <p className="informationText">In the field of music I am a clarinetist, pianist, composer, and educator. Throughout my career in music I have served many roles within different ensembles ranging from principal clarinetist, soloist, and concert master. Outside of the performing, I regularly compose music for a variety of ensembles and projects. As a composer I won the MTNA Young Artist Composition Competition in the State of Georgia and my music has contributed to the success of student film shocase winner "Payton's Place". These feats were all accomplished while I was actively teaching music. Most recently I was recognized by my school, Chukker Creek Elementary, as Teacher of the Year, an honor I take great pride in as it was awarded to me exactly a decade after I first began teaching.</p>
                                 </div>
-
-                                <h4 className="card__heading">
-                                    <span className="card__heading-span card__heading-span--1">
-                                        The Musician
-                                    </span>
-                                </h4>
+                                <a href="#popup" className="moreInfo">LISTEN</a>
                             </div>
-                            <div className="card__side card__side--back card__side--back-1">
-                                <div className="card__cta">
-                                    <div className="card__price-box">
-                                        <p className="card__price-only">In the field of music I am a clarinetist, pianist, composer, and educator. Throughout my career in music I have served many roles within different ensembles ranging from principal clarinetist, soloist, and concert master. Outside of the performing, I regularly compose music for a variety of ensembles and projects. As a composer I won the MTNA Young Artist Composition Competition in the State of Georgia and my music has contributed to the success of student film shocase winner "Payton's Place". These feats were all accomplished while I was actively teaching music. Most recently I was recognized by my school, Chukker Creek Elementary, as Teacher of the Year, an honor I take great pride in as it was awarded to me exactly a decade after I first began teaching.</p>
-                                    </div>
-                                    <a href="#popup" className="btn btn--white">LISTEN</a>
+                        </div>
+                        <div className="container">
+                            <div className="card programmer">
+                                <h1 className="title">Programmer</h1>
+                                <div className="information">
+                                    <p className="informationText">As a programmer, I love to solve problems. In developing new software and customizing websites fuels my need to be creative on a daily basis. Since beginning my journey into software development I have learned and found ways to implement a variety of technologies including HTML, CSS, Bootstrap, JavaScript, jQuery, API Git, Sass React, Node, Express, MongoDB, and SQL. I love the idea that with technology, my potential to impact others is simply greater and I want to use my knowledge to craft intuitive products that aid people with what their needs.</p>
                                 </div>
+                                <a href="#popup" className="moreInfo">MORE</a>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card life">
+                                <h1 className="title">Life</h1>
+                                <div className="information">
+                                    <p className="informationText">I have a wife, pets, and a need to enjoy life to its fullest. Outside of work I enjoy cooking, specifically refining recipes that I learn. Playing video games, particularly ones that have a narrative and require me to cooperate with other to overcome obstacles and achieve goals. Aside from this, I love go out with my family, including the pets, to different places and spending time together.</p>
+                                </div>
+                                <a href="#popup" className="moreInfo">LISTEN</a>
                             </div>
                         </div>
                     </div>
-    </div>
-                    </section>
-                    </div>
+        </>
   )
 }
